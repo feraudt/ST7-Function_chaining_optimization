d7e3eb8dd024a137ffbd2d0952c499186d07ec9a
Cizeron-Duret Thomas <thomas.duret@student-cs.fr>
2021-03-24 16:15:15 +0100

    Fct sous-graphe dans utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -22,3 +22,24 @@ def plot_graph(G):
     plt.plot()
     nx.draw(G, with_labels=True)
     plt.show()
+
+
+def bwd_sous_graph(G, bwd_seuil):
+    # On note les edge à supprimer
+    edge_to_delete = []
+    for u, v in G.edges():
+        if G.edges[u, v]['bandwidth'] < bwd_seuil:
+            edge_to_delete.append((u, v))
+    F = nx.Graph.copy(G)
+
+    # On les supprime de F
+    F.remove_edges_from(edge_to_delete)
+
+    # On enlève les sommets seuls
+    node_to_delete = []
+    for node in F.nodes():
+        if len(list(F.neighbors(node))) == 0:
+            node_to_delete.append(node)
+    F.remove_nodes_from(node_to_delete)
+
+    return F
